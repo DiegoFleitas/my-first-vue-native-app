dd7376902cb9f2968e04224ca34439f4ae175190
diegofleitas <diego.fleitas68@gmail.com>
2021-02-12 14:58:04 -0300

    :recycle: moved images to its own component

--- App.vue
+++ App.vue
@@ -2,22 +2,17 @@
    <view class="container">
       <text class="text-color-primary">{{ message }}</text>
       <button title="Press me!" @press="exclaim" />
-      <image
-          :source="require('./assets/favicon.png')"
-      />
-      <image
-          :style="{width: 50, height: 50}"
-          :source="{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}"
-      />
-      <image
-          :style="{width: 66, height: 58}"
-          :source="{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}"
-      />
+      <images></images>
    </view>
 </template>
 
 <script>
+import images from "./components/Images";
+
 export default {
+   components: {
+      images
+   },
    data() {
       return {
          message: "Hello World"
